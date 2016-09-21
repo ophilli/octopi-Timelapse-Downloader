8dd6c57303f37eec695a6c5d5d23a452b0978957
Owen Phillips <ophilli@clemson.edu>
2016-09-21 00:41:53 -0400

    Basic Rsync Template
    I've drafted a basic program structure to allow Rsync to cycle
    through all of the 3D printers and download their files.

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright: Owen Phillips 2016
+# Description: A simple backup script for octoprint timelapses.
+#              It is written with the Clemson Makerspace in mind,
+#              and serves as a general introduction to bash scripting.
+USER="****"
+PASS="****"
+HOSTS=("****" "****" "****" "****")
+RDIR="/home/octoprint/.octoprint/timelapse"
+LDIR="/mnt/MAKERSPACE/timelapse"
+
+for i in "{$HOSTS[*]}"; do
+    rsync -avzhe ssh $USER@$i:/$RDIR $LDIR/$i
+    echo $PASS
+done
+
+exit 0
