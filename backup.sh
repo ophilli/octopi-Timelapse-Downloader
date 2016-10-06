a4718b8495fb6b4b65f6577127b45812022a9f1f
Owen <ophilli@clemson.edu>
2016-10-05 23:08:41 -0400

    Handle Lulzbot OctoPi
    Due to the differences between TAM OctoPi and Stock OctoPi it is
    necessary to implement a slightly different rsync command for the
    Lulzbot.

--- backup.sh
+++ backup.sh
@@ -7,9 +7,13 @@ USER="****"
 HOSTS=("****" "****" "****" "****")
 RDIR="/home/octoprint/.octoprint/timelapse/"
 LDIR="/mnt/MAKERSPACE/timelapse"
+LULZ="****"
+LULZDIR="/home/pi/.octoprint/timelapse/"
 
 for i in "${HOSTS[@]}"; do
     rsync --remove-source-files -avzhe ssh $USER@$i:/$RDIR $LDIR/$i
 done
+
+rsync --exclude 'tmp' -rzhe 'ssh -q' pi@$LULZ:$LULZDIR $LDIR/$LULZ
 
 exit 0
