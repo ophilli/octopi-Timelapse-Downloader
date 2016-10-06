d1eec53484a20b1c0ea79ea87aa264f4380a00dd
Owen <ophilli@clemson.edu>
2016-10-05 23:15:06 -0400

    Implement Success and Failure Logging
    I have added commands that will write to disk the success and failure
    status of the timelapse sync for each machine.

--- backup.sh
+++ backup.sh
@@ -14,8 +14,21 @@ for i in "${HOSTS[@]}"; do
     rsync --exclude 'tmp' -rzhe 'ssh -q' $USER@$i:/$RDIR $LDIR/$i
     #rsync --remove-source-files -avzhe ssh $USER@$i:/$RDIR $LDIR/$i
 
+    if [ "$?" -eq "0" ]
+    then
+        echo "Backup of $i Successful: $(date)" >> ~/backup.log
+    else
+        echo "Backup of $i Failed: $(date)" >> ~/backup.log
+    fi
 done
 
 rsync --exclude 'tmp' -rzhe 'ssh -q' pi@$LULZ:$LULZDIR $LDIR/$LULZ
+
+if [ "$?" -eq "0" ]
+then
+    echo "Backup of $LULZ Successful: $(date)" >> ~/backup.log
+else
+    echo "Backup of $LULZ Failed: $(date)" >> ~/backup.log
+fi
 
 exit 0
